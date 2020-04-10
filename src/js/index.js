0781cd1ba41140f00fcc805769978224a9430611
mr <info@masujimaryohei.jp>
2020-04-10 18:28:52 +0900

    Add css files.

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+let output = document.getElementById("output");
+function mosaic() {
+    output.innerHTML = 'My mosaic icon.<div class=\'mosaic\'></div>';
+}
+
+function photoOriginal() {
+    output.innerHTML = 'My original photo icon.<div class=\'photo-origin\'></div>';
+}
+
+function photoWithRed() {
+    output.innerHTML = 'My photo icon with red.<div class=\'photo-red\'></div>';
+}
